c853955ba58fdf21af4032634564b42fc11364d9
Chan647 <cksgml8760@gmail.com>
2025-10-31 17:20:05 +0900

    날씨 정보 받아오기 추가 및 최종 수정

--- watersys.ino
+++ watersys.ino
@@ -2,7 +2,7 @@
 #include <SoftwareSerial.h>
 #include "DHT.h"
 
-#define DEBUG true
+#define DEBUG false
 #define DHTTYPE DHT11 
 #define DHTPIN 2  
 #define AA  3
@@ -32,6 +32,7 @@ bool buzcon = LOW;
 bool manual_mode = false;
 bool manual_pump = false;
 bool pumpOn = false;
+bool tpumpOn = false;
 
 unsigned long last_manual = 0;
 unsigned long lastdisplay = 0;
@@ -39,35 +40,26 @@ unsigned long lastbuzz = 0;
 unsigned long lastled = 0;
 unsigned long lastsoil = 0;
 unsigned long lastsend = 0;
-unsigned long lastDhtRead = 0;
+unsigned long lastDhTread = 0;
+unsigned long lastsmsg = 0;
+unsigned long lastwmsg = 0;
 
-const int low_water = 100;  
-const int nor_water = 150;  
-const int low_soil  = 600;  
-const int nor_soil  = 580;
+unsigned long boot_t0 = 0;
 
-SoftwareSerial esp(12,13);
+SoftwareSerial esp8266(12,13);
 
-String ssid = "JJanni";
-String pass = "1q2w3e4r";
-String apiKey = "803d087ba957880e946afca96308e974";   
+String ssid = "202호 강의실 2.4";  
+String password = "4719222202";     
+String apiKey = "273cf286f730b3ade1525fd0361770cb"; 
 String city = "Seoul,KR";
-
-String sendData(String cmd, int wait) {
-  String res = "";
-  esp.print(cmd);
-  long t = millis();
-  while (millis() - t < wait) {
-    while (esp.available()) res += (char)esp.read();
-  }
-  Serial.print(res);
-  return res;
-}
+String ans = " ";
 
 
 void setup() {
   pinMode(AA, OUTPUT);
   pinMode(AB, OUTPUT);
+  pinMode(BA, OUTPUT);
+  pinMode(BB, OUTPUT);
   pinMode(RLED, OUTPUT);
   pinMode(BLED, OUTPUT);
   pinMode(GLED, OUTPUT);
@@ -77,28 +69,49 @@ void setup() {
   lcd.backlight();  
 
   Serial.begin(9600);
-  dht.begin();
-  esp.begin(9600);
-  delay(2000);
+  esp8266.begin(9600);
 
-  Serial.println("\n[ESP8266 초기화]");
-  sendData("AT\r\n", 1000);
-  sendData("AT+RST\r\n", 2000);
-  sendData("ATE0\r\n", 1000);
-  sendData("AT+CWMODE=1\r\n", 1000);
-  delay(1000);
-  sendData("AT+CWJAP=\"" + ssid + "\",\"" + pass + "\"\r\n", 15000);
-  delay(3000);
-  sendData("AT+CIPRECVMODE=0\r\n", 1000);
-  sendData("AT+CIFSR\r\n", 1000);
+  Serial.print("Please Wait...");
+  sendData("AT+RST\r\n", 2000, DEBUG);
+  sendData("AT+CWMODE=1\r\n", 1000, DEBUG);
+  sendData("AT+CWJAP=\"" + ssid + "\",\"" + password + "\"\r\n", 6000, DEBUG);
 
-  delay(2000);
-  getWeather();
+  Serial.print("Please Wait 5 more second...");
+  delay(5000);
+  Serial.print("WiFi Connected! ");
+
+  
+  sendData("AT+CIFSR\r\n",1000,DEBUG);
+
+
+  String cm = "AT+CIPSTART=\"TCP\",\"api.openweathermap.org\",80\r\n";
+  sendData(cm, 3000, DEBUG);
+
+
+  String getStr = "GET /data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric HTTP/1.1\r\n"
+                  "Host: api.openweathermap.org\r\n"
+                  "Connection: close\r\n\r\n";
+
+  
+  cm = "AT+CIPSEND=" + String(getStr.length()) + "\r\n";
+  sendData(cm, 20000, DEBUG);
+
+
+  String response = sendData(getStr, 20000, DEBUG);
+  String weather;
+  int start = response.indexOf("main");
+  int end = response.indexOf("\",");
+  weather = response.substring(start + 7, end);
+  Serial.println(response);
+  Serial.print("Tomorrow Weather: ");
+  Serial.println(weather);
+  ans = weather;
 }
+
 void on_waterpump(){
   digitalWrite(AA, HIGH);
   digitalWrite(AB, LOW);
-  pumpOn = true;
+  pumpOn = false;
 }
 
 void off_waterpump(){
@@ -107,15 +120,32 @@ void off_waterpump(){
   pumpOn = false;
 }
 
+void on_tankpump(){
+  digitalWrite(BA, HIGH);
+  digitalWrite(BB, LOW);
+  tpumpOn = false;
+}
+
+void off_tankpump(){
+  digitalWrite(BA,LOW);
+  digitalWrite(BB,LOW);
+  tpumpOn = false;
+}
+
 void readDHT_safely() {
   bool wasPumpOn = pumpOn; 
   if (wasPumpOn) off_waterpump();
-  delay(30);           
+  delay(30);       
+
+  bool wastPumpOn = tpumpOn;
+  if (wastPumpOn) off_tankpump();
+  delay(30);    
 
   float h = dht.readHumidity();
   float t = dht.readTemperature();
 
   if (wasPumpOn) on_waterpump(); 
+  if (wastPumpOn) on_tankpump();
 
   if (!isnan(h) && !isnan(t)) {
     lastH = h; lastT = t;       
@@ -123,17 +153,6 @@ void readDHT_safely() {
 
 }
 
-void on_tankpump(){
-  digitalWrite(BA, HIGH);
-  digitalWrite(BB, LOW);
-  delay(3000);
-}
-
-void off_tankpump(){
-  digitalWrite(BA,LOW);
-  digitalWrite(BB,LOW);
-  delay(3000);
-}
 
 void show_sen(const char* s1, const char* s2){
   lcd.setCursor(0,0); lcd.print(s1);
@@ -156,6 +175,23 @@ void off_led(){
   digitalWrite(BLED, LOW);
 }
 
+String sendData(String command, const int timeout, boolean debug) {
+  String response = "";
+  esp8266.print(command);
+  long int time = millis();
+  while ((millis() - time) < timeout) {
+    while (esp8266.available()) {
+      char c = esp8266.read();
+      response += c;
+    }
+  }
+  if (debug) {
+    Serial.print(response);
+  }
+  Serial.println();
+  return response;
+}
+
 
 void loop() { 
   unsigned long now = millis();
@@ -170,15 +206,16 @@ void loop() {
   float wat_lv = waterlevel;
   float soil_moi = soilmoisture; 
 
-  if (now - lastDhtRead >= 2000) {
-  lastDhtRead = now;
+  if (now - lastDhTread >= 2000) {
+  lastDhTread = now;
   readDHT_safely();
-}
+  }
 
 
   if (cmd == "AUTO") {
     manual_mode = false;
     off_waterpump();
+    off_tankpump();
     Serial.println("MODE_AUTO");
   }
   else if (cmd == "MANUAL") {
@@ -191,26 +228,41 @@ void loop() {
     String cmd = Serial.readStringUntil('\n');
     cmd.trim();
 
-    if (cmd == "W1") { 
-        manual_mode = true;
-        manual_pump = true;
-        last_manual = millis();
-        on_waterpump();
-        Serial.println("WATERPUMP_ON");
-    }
-    else if (cmd == "W0") { 
+    if (cmd == "W0") { 
         manual_mode = true;
         manual_pump = false;
         last_manual = millis();
         off_waterpump();
-        Serial.println("WATERPUMP_OFF");
-    }
   }
+    
+
+    else if (cmd == "W1") { 
+        manual_mode = true;
+        manual_pump = true;
+        last_manual = millis();
+        on_waterpump();
+    }
+
+    else if (cmd == "W2") { 
+        manual_mode = true;
+        manual_pump = true;
+        last_manual = millis();
+        on_tankpump();
+  }
+
+  else if (cmd == "W3") { 
+        manual_mode = true;
+        manual_pump = true;
+        last_manual = millis();
+        off_tankpump();
+  }
+}
 
   if (manual_mode) {
     if (millis() - last_manual > 30000) {
         manual_mode = false;
         off_waterpump();
+        off_tankpump();
         Serial.println("AUTO_MODE_RESUME");
     }
     else {
@@ -226,22 +278,25 @@ void loop() {
     lastsoil = now;
   }
 
-  if (now - lastsend >= 1000){
-    lastsend = now;
-    if(isnan(lastT) || isnan(lastH)) return; 
-    
-    Serial.print(lastT); Serial.print(',');
-    Serial.print(lastH); Serial.print(',');
-    Serial.print(mm); Serial.print(',');
-    Serial.print(per); Serial.print('\n');
+  if (now - lastsend >= 30000){
+    if(ans.length() > 2){
+      lastsend = now;
+      if(isnan(lastT) || isnan(lastH)) return;
+
+      Serial.print("DATA :");
+      Serial.print(lastT); Serial.print(',');
+      Serial.print(lastH); Serial.print(',');
+      Serial.print(mm); Serial.print(',');
+      Serial.print(per); Serial.print('\n');
+    }
   }
   
 
-  if (mode != 2 && per <= 40) {
+  if (mode != 2 && per <= 50) {
     mode = 2;  
   }
-  else if (mode == 2 && per >= 45) {
-    if (waterlevel <= low_water){
+  else if (mode == 2 && per > 60) {
+    if (mm <= 20){
       mode = 1;
     }
     else{
@@ -249,19 +304,19 @@ void loop() {
     }  
   }
   else {
-    if (mode != 1 && waterlevel <= low_water) {
+    if (mode != 1 && mm <= 20) {
       mode = 1;
     }
-    else if (mode == 1 && waterlevel >= nor_water) {
+    else if (mode == 1 && mm > 25) {
       mode = 0;
     }
   }
  
    switch (mode) {
     case 0: 
+      off_led();
       off_waterpump();
       off_tankpump();
-      off_led();
       noTone(BUZZER);
 
       if (now - lastdisplay >= 2000) {
@@ -271,97 +326,85 @@ void loop() {
       }
 
         switch (screenID) {
+          
           case 0:
-            show_val("Soil Moisture",per);
-            lcd.print("%");
-            break;
-
-          case 1:
             show_val("Temperature",lastT);
             lcd.write(byte(223));
             lcd.print("C");
             break;
 
-          case 2:
+          case 1:
             show_val("Humidity",lastH);
             lcd.print("%");
             break;
 
-          case 3:
-            show_val("Water Level",waterlevel);
+          case 2:
+            show_val("Water Level",mm);
             break;
+
+          case 3:
+            show_val("Soil Moisture",per);
+            lcd.print("%");
+            break;
+
         }
       break;
 
     case 1:
-       off_waterpump();
        on_tankpump();
+       off_waterpump();
        noTone(BUZZER);
-       off_led();
-
+       show_sen("Water Shortage!","Fill the Bottle!");
+      
+       
+       if (now - lastsend >= 30000){
+        lastsend = now;
+        if(isnan(lastT) || isnan(lastH)) return;
+        
+        Serial.print(lastT); Serial.print(',');
+        Serial.print(lastH); Serial.print(',');
+        Serial.print(mm); Serial.print(',');
+        Serial.print(per); Serial.print('\n');
+        }
+      
        if (now - lastled >= 500) {
         lastled = now;
         ledcon = !ledcon;
-        digitalWrite(RLED, ledcon);
+        digitalWrite(RLED, LOW);
         digitalWrite(GLED, ledcon);
         digitalWrite(BLED, LOW);
-        reset_sen();
-        show_sen("Water Shortage!","Fill the Bottle!");
         break;
        }
       break;
 
     case 2:
-      on_waterpump();
-      if (now - lastsoil >= 500) {
+        on_waterpump();
+        show_sen("Soil is Dry!","Water the Plants!");
+
+      if (now - lastsend >= 30000){
+        lastsend = now;
+        if(isnan(lastT) || isnan(lastH)) return;
         
+        Serial.print(lastT); Serial.print(',');
+        Serial.print(lastH); Serial.print(',');
+        Serial.print(mm); Serial.print(',');
+        Serial.print(per); Serial.print('\n');
+        }
+
+      if (now - lastsoil >= 500) {
         lastsoil= now;
         ledcon = !ledcon;
-        digitalWrite(RLED, HIGH);
+        digitalWrite(RLED, ledcon);
         digitalWrite(GLED, LOW);
         digitalWrite(BLED, LOW);
-        reset_sen();
-        show_sen("Soil is Dry!","Water the Plants!");
+      }
+
+      if (millis() - lastbuzz >= 1000){
+        tone(BUZZER, tones, 200);
+        lastbuzz = millis();
         break;
       }
       break;
   }
-
 }
 
-void getWeather() {
-  String host = "api.openweathermap.org";
-  String url = "/data/2.5/forecast?q=" + city + "&appid=" + apiKey + "&units=metric&lang=kr";
-
-  sendData("AT+CIPSTART=\"TCP\",\"" + host + "\",80\r\n", 5000);
-
-  String req = "GET " + url + " HTTP/1.1\r\nHost: " + host + "\r\nConnection: close\r\n\r\n";
-  sendData("AT+CIPSEND=" + String(req.length()) + "\r\n", 2000);
-  esp.print(req);
-
-  delay(10000);  
-
-  String res = "";
-  while (esp.available()) res += (char)esp.read();
-  sendData("AT+CIPCLOSE\r\n", 1000);
-
-  String clean = "";
-  for (int i = 0; i < res.length(); i++) {
-    char c = res[i];
-    if (c >= 32 && c <= 126) clean += c;
-  }
-
-  int idx = clean.indexOf("\"main\":\"");
-  if (idx > 0) {
-    int s = idx + 8;
-    int e = clean.indexOf('"', s);
-    String desc = clean.substring(s, e);
-    Serial.println("\n☀ 내일 날씨: " + desc); 
-    if (desc == "Rain"){
-    off_waterpump();
-    }
-    mode = 0; 
-  }
-  else {
-    mode = 0;  } 
-}
